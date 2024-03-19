868681bad45be99f76342b06a1712a21ab837d79
iqrakhann <160530247+iqrakhann@users.noreply.github.com>
2024-03-20 01:36:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// string
+// number
+// boolean=>true and false
+//null
+//undefined
+//symbol
+//bigint
+//data structure
+//object
+//array
+//string
+var personName = ("IQRA");
+console.log(personName);
+//Number
+var personAge = 45;
+var finalAge = +4;
+console.log("finalAge," + personAge);
+//boolean 
+var boolean = true;
+console.log(boolean);
+var a;
+console.log(null);
+//undefined
+var b;
+console.log(undefined);
+//Arry
+var Name = ["iqra Tariq"];
+console.log(Name);
+//SYMBOL
+var SYMBOL = ("Hello");
+console.log(Symbol);
---
+++ index.ts
@@ -0,0 +1,62 @@
+// string
+// number
+// boolean=>true and false
+//null
+//undefined
+//symbol
+//bigint
+
+//data structure
+ 
+//object
+//array
+
+//string
+let personName1:string = "IQRA";
+console.log(personName);
+
+//Number
+
+let personAge:Number= 45;
+let finalAge:Number= +4;
+console.log("finalAge," + personAge);
+
+//boolean 
+
+let boolean:boolean=true;
+console.log(boolean);
+let a:null;
+console.log(null);
+
+//undefined
+
+let b:undefined;
+
+console.log(undefined);
+
+//Arry
+
+let Name1:string[]=["iqra Tariq"];
+
+console.log(Name);
+
+//SYMBOL
+
+let SYMBOL= ("Hello");
+console.log("Hello");
+
+// **OBJECT ARRAY**
+// NESTED ARRY
+let arry = [{},{},[],[],true,"a"]
+let objec = {
+    key: {
+
+    },key2: []
+
+}
+
+
+
+let user:{
+    fullName.
+}
